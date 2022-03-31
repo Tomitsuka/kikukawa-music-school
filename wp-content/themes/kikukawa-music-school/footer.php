f48812b0dfad88925f25ca1b216265f22369f5e1
Tomitsuka <and.so.on.1011@gmail.com>
2022-03-31 21:59:34 +0900

    フッター&リスト&ヘッダー修正

--- wp-content/themes/kikukawa-music-school/footer.php
+++ wp-content/themes/kikukawa-music-school/footer.php
@@ -4,12 +4,29 @@
 				<a href="<?php echo esc_url(home_url('/')); ?>"><img alt="菊川音楽教室" src="<?php echo get_template_directory_uri(); ?>/images/logo_white.svg" /></a>
 			</div>
 			<div class="footer-info__box">
-				<div class="footer-info__item">住所 : [ 茅ヶ崎校 ] 〒253-0072 神奈川県茅ヶ崎市今宿785-8</div>
-				<div class="footer-info__item">　　　[ 津田沼校 ] 〒274-0825 千葉県船橋市前原西1-23-7-104</div>
-				<div class="footer-info__item">営業時間 : 10:00〜21:00</div>
-				<div class="footer-info__item">お問い合わせ : <a href="https://forms.gle/hYhretfVKwrViTqp9" target="_blank">https://forms.gle/hYhretfVKwrViTqp9</a></div>
-				<div class="footer-info__item">電話番号 : 090-6168-2950</div>
-				<div class="footer-info__text">※レッスン中など、お電話に出られない場合がございます。<br>繋がらない場合は、留守番電話にメッセージを入れてください。後ほど折り返しお電話させていただきます。</div>
+				<div class="footer-info__item footer-info__address">
+					<div class="footer-info__label">住所</div>
+					<div class="footer-info__text">
+						<div class="address__item">[ 茅ヶ崎校 ] <br class="sp"> 〒253-0072 神奈川県茅ヶ崎市今宿785-8</div>
+						<div class="address__item">[ 津田沼校 ] <br class="sp"> 〒274-0825 千葉県船橋市前原西1-23-7-104</div>
+					</div>
+				</div>
+				<div class="footer-info__item">
+					<div class="footer-info__label">営業時間</div>
+					<div class="footer-info__text">
+						10:00〜21:00
+					</div>
+				</div>
+				<div class="footer-info__item">
+					<div class="footer-info__label">電話番号</div>
+					<div class="footer-info__text">
+						090-6168-2950
+					</div>
+				</div>
+				<div class="footer-info__notes">※レッスン中など、お電話に出られない場合がございます。<br>繋がらない場合は、留守番電話にメッセージを入れてください。後ほど折り返しお電話させていただきます。</div>
+				<div class="footer-info__contact">
+					<a href="https://forms.gle/hYhretfVKwrViTqp9" target="_blank">お問い合わせ</a>
+				</div>
 			</div>
 			<div class="footer_copyright">
 				<small>© KIKUKAWA MUSIC SCHOOL all rights reserved.</small>
